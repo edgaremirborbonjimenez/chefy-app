cd3b91ab5b0c4a747de6b5174d9688dca3a82d85
SnakeEater007 <edemboji@gmail.com>
2023-09-16 12:52:30 -0700

    Genere el controller de Orders y su service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrdersController } from './orders/orders.controller';
+import { OrdersService } from './orders/orders.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, OrdersController],
+  providers: [AppService, OrdersService],
 })
 export class AppModule {}
