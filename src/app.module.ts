6f91d3abdd3cd5f1829c1fe9337919a11d35b78e
OscarMinjarez <oscarminjarez@outlook.com>
2023-09-17 17:19:04 -0700

    Se mapeo Order y se instaló typeorm correctamente

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,20 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrdersModule } from './orders/orders.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmOptions } from './configs/typeorm.config';
 
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'eebj031002',
-      database: 'chefy',
-      entities:[ __dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true
-    }),
+    TypeOrmModule.forRoot(TypeOrmOptions),
     OrdersModule],
   controllers: [AppController],
   providers: [AppService],
