4a73f271ff4a2db65370a36a9acb63a59a47931d
DanielPenaGarcia <dapgpena@gmail.com>
2023-09-17 17:39:04 -0700

    Se creó constructor en el controlador orders y su acción GET

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('orders')
-export class OrdersController {}
+export class OrdersController {
+
+    constructor(){
+
+
+    }
+
+    @Get()
+    getOrders(){
+
+    }
+
+}
