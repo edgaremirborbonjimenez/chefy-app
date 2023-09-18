399b1e879c2dae489bcdd0b5bce77d67d40e3a77
DanielPenaGarcia <dapgpena@gmail.com>
2023-09-17 17:47:46 -0700

    Se realizó instancia de order service, y se esperan cambios

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,16 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
+import { OrdersService } from './orders.service';
 
 @Controller('orders')
 export class OrdersController {
 
-    constructor(){
-
-
+    constructor(private orderServcie: OrdersService){
     }
 
     @Get()
     getOrders(){
-
+        return //this.orderServcie.getOrders();
     }
 
 }
