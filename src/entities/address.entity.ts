25222f44e7d6909bf4e49ef60d1d224a1267680e
OscarMinjarez <oscarminjarez@outlook.com>
2023-09-24 09:59:24 -0700

    Se mapeó la entidad contact y address en order

---
+++ src/entities/address.entity.ts
@@ -0,0 +1,11 @@
+import { Column, Entity } from "typeorm";
+import AbstractEntity from "./abstract.entity";
+
+@Entity({ name: "Addresses" })
+export class Address extends AbstractEntity {
+    @Column({ nullable: false, type: "varchar", length: 200 })
+    streetAddress: string;
+
+    @Column({ nullable: false, type: "varchar", length: 5 })
+    zipCode: string;
+}
--- src/entities/order.entity.ts
+++ src/entities/order.entity.ts
@@ -1,8 +1,12 @@
-import { Column, Entity } from "typeorm";
+import { Column, Entity, ManyToOne } from "typeorm";
 import AbstractEntity from "./abstract.entity";
+import { Contact } from "./contact.entity";
 
 @Entity({ name: "orders" })
 export class Order extends AbstractEntity {
     @Column({ nullable: false, type: "varchar", length: 20 })
     status: string;
+
+    @ManyToOne(() => Contact, (contact) => contact.id)
+    contact: Contact;
 }
