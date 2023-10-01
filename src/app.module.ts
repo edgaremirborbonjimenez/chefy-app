9725517d699513d5bd38af0396904224209090a3
SnakeEater007 <edemboji@gmail.com>
2023-10-01 15:17:07 -0700

    Se arreglo el error de los modulos de las entidades para generar la base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,18 @@ import { AppService } from './app.service';
 import { OrdersModule } from './orders/orders.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmOptions } from './configs/typeorm.config';
+import { ContactsModule } from './contacts/contacts.module';
+import { DishesModule } from './dishes/dishes.module';
+import { IngredientsModule } from './ingredients/ingredients.module';
 
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(TypeOrmOptions),
-    OrdersModule],
+    OrdersModule,
+    ContactsModule,
+    DishesModule,
+    IngredientsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Order } from '../entities/order.entity';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
+import { Contact } from 'src/entities/contact.entity';
+import { Dish } from 'src/entities/dish.entity';
 
 @Module({
-    imports:[TypeOrmModule.forFeature([Order])],
+    imports:[TypeOrmModule.forFeature([Order,Contact,Dish])],
     controllers: [OrdersController],
     providers: [OrdersService],
 })
